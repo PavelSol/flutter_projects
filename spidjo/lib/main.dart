73d924e5f37534e8a7306adf6c108d9d94c8c51a
Pasha <p.n.soldatenko@gmail.com>
2023-12-22 20:42:57 +0300

    spidjo

--- spidjo/lib/main.dart
+++ spidjo/lib/main.dart
@@ -1,125 +1,323 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_joystick/flutter_joystick.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const ballSize = 20.0;
+const step = 10.0;
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Joystick Example'),
+        ),
+        body: const MainPage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const JoystickExample()),
+              );
+            },
+            child: const Text('Joystick'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const JoystickAreaExample()),
+              );
+            },
+            child: const Text('Joystick Area'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SquareJoystickExample()),
+              );
+            },
+            child: const Text('Square Joystick'),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class JoystickExample extends StatefulWidget {
+  const JoystickExample({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  State<JoystickExample> createState() => _JoystickExampleState();
+}
+
+class _JoystickExampleState extends State<JoystickExample> {
+  double _x = 100;
+  double _y = 100;
+  JoystickMode _joystickMode = JoystickMode.all;
+
+  @override
+  void didChangeDependencies() {
+    _x = MediaQuery.of(context).size.width / 2 - ballSize / 2;
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.green,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Joystick'),
+        actions: [
+          JoystickModeDropdown(
+            mode: _joystickMode,
+            onChanged: (JoystickMode value) {
+              setState(() {
+                _joystickMode = value;
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.green,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Ball(_x, _y),
+            Align(
+              alignment: const Alignment(0, 0.8),
+              child: Joystick(
+                mode: _joystickMode,
+                listener: (details) {
+                  setState(() {
+                    _x = _x + step * details.x;
+                    _y = _y + step * details.y;
+                  });
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class JoystickAreaExample extends StatefulWidget {
+  const JoystickAreaExample({Key? key}) : super(key: key);
+
+  @override
+  State<JoystickAreaExample> createState() => _JoystickAreaExampleState();
+}
+
+class _JoystickAreaExampleState extends State<JoystickAreaExample> {
+  double _x = 100;
+  double _y = 100;
+  JoystickMode _joystickMode = JoystickMode.all;
+
+  @override
+  void didChangeDependencies() {
+    _x = MediaQuery.of(context).size.width / 2 - ballSize / 2;
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: const Text('Joystick Area'),
+        actions: [
+          JoystickModeDropdown(
+            mode: _joystickMode,
+            onChanged: (JoystickMode value) {
+              setState(() {
+                _joystickMode = value;
+              });
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: JoystickArea(
+          mode: _joystickMode,
+          initialJoystickAlignment: const Alignment(0, 0.8),
+          listener: (details) {
+            setState(() {
+              _x = _x + step * details.x;
+              _y = _y + step * details.y;
+            });
+          },
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.green,
+              ),
+              Ball(_x, _y),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SquareJoystickExample extends StatefulWidget {
+  const SquareJoystickExample({Key? key}) : super(key: key);
+
+  @override
+  State<SquareJoystickExample> createState() => _SquareJoystickExampleState();
+}
+
+class _SquareJoystickExampleState extends State<SquareJoystickExample> {
+  double _x = 100;
+  double _y = 100;
+  JoystickMode _joystickMode = JoystickMode.all;
+
+  @override
+  void didChangeDependencies() {
+    _x = MediaQuery.of(context).size.width / 2 - ballSize / 2;
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: const Text('Square Joystick'),
+        actions: [
+          JoystickModeDropdown(
+            mode: _joystickMode,
+            onChanged: (JoystickMode value) {
+              setState(() {
+                _joystickMode = value;
+              });
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.green,
+            ),
+            Ball(_x, _y),
+            Align(
+              alignment: const Alignment(0, 0.8),
+              child: Joystick(
+                mode: _joystickMode,
+                base: JoystickSquareBase(mode: _joystickMode),
+                stickOffsetCalculator: const RectangleStickOffsetCalculator(),
+                listener: (details) {
+                  setState(() {
+                    _x = _x + step * details.x;
+                    _y = _y + step * details.y;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class JoystickModeDropdown extends StatelessWidget {
+  final JoystickMode mode;
+  final ValueChanged<JoystickMode> onChanged;
+
+  const JoystickModeDropdown(
+      {Key? key, required this.mode, required this.onChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16.0),
+        child: FittedBox(
+          child: DropdownButton(
+            value: mode,
+            onChanged: (v) {
+              onChanged(v as JoystickMode);
+            },
+            items: const [
+              DropdownMenuItem(
+                  value: JoystickMode.all, child: Text('All Directions')),
+              DropdownMenuItem(
+                  value: JoystickMode.horizontalAndVertical,
+                  child: Text('Vertical And Horizontal')),
+              DropdownMenuItem(
+                  value: JoystickMode.horizontal, child: Text('Horizontal')),
+              DropdownMenuItem(
+                  value: JoystickMode.vertical, child: Text('Vertical')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ball extends StatelessWidget {
+  final double x;
+  final double y;
+
+  const Ball(this.x, this.y, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: x,
+      top: y,
+      child: Container(
+        width: ballSize,
+        height: ballSize,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.redAccent,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 3),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
